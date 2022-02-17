cd362459992c2197ae2a2c62578432292b5985d8
Asif Seferov <asif_seferov_1995@mail.ru>
2022-02-18 00:23:17 +0400

    created relation with tables

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Category;
+//use Cviebrock\EloquentSluggable\Sluggable;
 
 class MainController extends Controller
 {
     public function index(){
+        Category::create([
+            'name' => 'Salam Dünya'
+        ]);
         return view('admin.index');
     }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Tag extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+    protected $guarded = [];
+   
+
+    public function posts(){
+        return $this->belongsToMany(Post::class);
+    }
 }
